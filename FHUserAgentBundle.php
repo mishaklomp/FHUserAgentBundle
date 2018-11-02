13f5e36f222d74a9ed140c936243f8281a8ee0fb
Evert Harmeling <evert.harmeling@freshheads.com>
2018-11-02 09:33:17 +0100

    Initial commit

---
+++ FHUserAgentBundle.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace FH\Bundle\UserAgentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Evert Harmeling <evert@freshheads.com>
+ */
+class FHUserAgentBundle extends Bundle
+{
+}
